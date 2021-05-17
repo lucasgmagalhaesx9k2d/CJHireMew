88de2eb05c12506b34373d589fd0c07f725a571f
lucasgmagalhaesx9k2d <lianguxh61949@yeah.net>
2021-05-18 07:43:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jeremyfrey/serviceapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.jeremyfrey.serviceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonLawn, buttonPool, buttonOther;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonLawn = (Button) findViewById(R.id.button_Lawn);
+        buttonLawn.setOnClickListener(this);
+        buttonPool = (Button) findViewById(R.id.button_Pool);
+        buttonPool.setOnClickListener(this);
+        buttonOther = (Button) findViewById(R.id.button_Other);
+        buttonOther.setOnClickListener(this);
+    }
+
+    public void homeView(View v){
+        if(v.equals(buttonLawn)) {
+            //lawn view
+            setContentView(R.layout.activity_lawn_tiles);
+            //buttonPool.setVisibility(View.INVISIBLE);
+            //buttonOther.setVisibility(View.INVISIBLE);
+            //Toast.makeText(getApplicationContext(), "lawn view...",
+                    //Toast.LENGTH_SHORT).show();
+        }
+
+        else if(v.equals(buttonPool)) {
+            //pool View
+            setContentView(R.layout.activity_pool_tiles);
+            //buttonLawn.setVisibility(View.INVISIBLE);
+            //buttonOther.setVisibility(View.INVISIBLE);
+            //Toast.makeText(getApplicationContext(), "pool view...",
+                    //Toast.LENGTH_SHORT).show();
+        }
+        else if(v.equals(buttonOther)){
+            //other View
+            buttonPool.setVisibility(View.INVISIBLE);
+            buttonLawn.setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(), "other view...",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else {
+            setContentView(R.layout.activity_main);
+        }
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        homeView(v);
+    }
+}
